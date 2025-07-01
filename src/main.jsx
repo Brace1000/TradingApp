7928482b64cc58f9e1f27bad4f8eab6a2c5cc49e
bobaigwa <oikobrian99@gmail.com>
2025-07-01 13:55:29 +0300

    chore: initialize ReactDOM root and render App with global styles

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Make sure the extension is here
+import './index.css';     // Make sure this path is correct
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
